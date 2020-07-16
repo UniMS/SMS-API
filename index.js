a808b389ecab2539168eb464febbc106b916a96d
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-07-16 22:57:20 +0630

    configure environment variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-require("./startup/db")();
+require("./database/connection");
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}...`));
