f4abb1548862269482a8fc0b309521517bb95a91
PhyaeSoneThwim <phyaesonethwim123@gmail.com>
2020-07-26 18:45:49 +0630

    create new student

--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.post("/login");
-router.route("/").get().post();
-router.route("/:userId").get().patch().delete();
-
-module.exports = router;
--- src/startup/router.js
+++ src/startup/router.js
@@ -4,7 +4,6 @@ const user = require("../routes/user");
 module.exports = function (app) {
   app.use("/api/roles", role);
   app.use("/api/students", student);
-  app.use("/api/users", user);
 
   app.all("*", (req, res, next) => {
     return res.status(404).json({
