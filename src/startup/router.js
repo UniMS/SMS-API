c1e91cabf25f13b689cb2b09577f36eaa73be9c5
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-08-16 23:55:44 +0630

    deleted role :fire:

--- src/startup/router.js
+++ src/startup/router.js
@@ -1,10 +1,8 @@
-const role = require("../routes/role");
 const student = require("../routes/student");
 const grading = require("../routes/grading");
 const statistic = require("../routes/statistic");
 
 module.exports = function (app) {
-  app.use("/api/roles", role);
   app.use("/api/students", student);
   app.use("/api/gradings", grading);
   app.use("/api/statistics", statistic);
