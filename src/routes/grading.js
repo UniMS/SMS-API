f8f2dabcd3552f5e6cb73c98698f95e3e11f2027
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-08-19 11:52:23 +0630

    wrote docs :books:

--- src/routes/grading.js
+++ src/routes/grading.js
@@ -2,56 +2,63 @@ const express = require("express");
 const router = express.Router();
 const gradings = require("../controllers/gradings");
 
+/**
+--------------------------------------------
+Filter Gradings
+--------------------------------------------
+ * @filterGradings returns lists of gradings which is respect to given academic year, major and attendance year.
+ */
 router.get(
   "/academic-year/:academicYearId/major/:majorId/attendance-year/:attendanceYearId",
   gradings.filterGradings
 );
 
-/*
+/**
 --------------------------------------------
-GPA
+Final Year GPA
 --------------------------------------------
-*/
+ * @getFinalYearGPA generates final year GAP.
+ */
 router.get(
   "/academic-year/:academicYearId/roll-no/:rollNo/gpa",
   gradings.getFinalYearGPA
 );
 
+/**
+--------------------------------------------
+CumulativeGPA
+--------------------------------------------
+ * @getCumulativeGPA generates cumulative GAP which is all-academic-year-GPA.
+ */
 router.get(
   "/academic-year/:academicYearId/roll-no/:rollNo/cumulative-gpa",
   gradings.getCumulativeGPA
 );
 
-/*
+/**
 --------------------------------------------
 Grading Document
 --------------------------------------------
-*/
+ * @generateGradings generates grading document.
+ * * 1 - for from-academic-year to to-academic-year -> /degree/1/from/2/to/4/roll-no/4IST-44
+ * * 2 - for x-academic-year                        -> /degree/1/from/2/to/2/roll-no/3IST-33
+ */
 router.get(
   "/degree/:degreeId/from/:fromAcademicYearId/to/:toAcademicYearId/roll-no/:rollNo",
   gradings.generateGradings
 );
-// 1 year - x year -> /degree/1/from/1/to/4/roll-no/4IST-44/all-marks
-// x year - x year -> /degree/1/from/2/to/4/roll-no/4IST-44/all-marks
-// 2 year          -> /degree/1/from/2/to/2/roll-no/3IST-33/all-marks
 
-/*
+/**
 --------------------------------------------
 Marks Certificate
 --------------------------------------------
-*/
+ * @generateMarks generates mark certificate.
+ * * 1 - for from-academic-year to to-academic-year -> /degree/1/from/2/to/4/roll-no/4IST-44/all-marks
+ * * 2 - for x-academic-year                        -> /degree/1/from/2/to/2/roll-no/3IST-33/all-marks
+ */
 router.get(
   "/degree/:degreeId/from/:fromAcademicYearId/to/:toAcademicYearId/roll-no/:rollNo/all-marks",
   gradings.generateMarks
 );
-// 1 year - x year -> /degree/1/from/1/to/4/roll-no/4IST-44/all-marks
-// x year - x year -> /degree/1/from/2/to/4/roll-no/4IST-44/all-marks
-// 2 year          -> /degree/1/from/2/to/2/roll-no/3IST-33/all-marks
-/*
---------------------------------------------
-Basic CRUD
---------------------------------------------
-*/
-router.get("/students/:studentId", gradings.getGradingsByStudentId);
 
 module.exports = router;
