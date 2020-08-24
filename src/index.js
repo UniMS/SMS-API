33b740ddba08d7917865cd64b4af65899e2b0f10
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-08-24 14:55:26 +0630

    removed body-parser :fire:

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-
-require("./startup/router")(app);
-require("./middlewares/error");
+require('./startup/router')(app);
+require('./middlewares/error');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}...`));
