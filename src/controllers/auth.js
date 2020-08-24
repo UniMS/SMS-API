61d3b47f3ac8f6deb91e45fbde9b7bc042198cea
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-08-24 14:54:19 +0630

    added jwt and auth :tada:

---
+++ src/controllers/auth.js
@@ -0,0 +1,30 @@
+const _ = require('lodash');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const models = require('../database/models');
+const catchAsync = require('../utils/catchAsync');
+
+exports.login = catchAsync(async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).json({ status: 'fail', message: error.details[0].message });
+
+  let user = await models.User.findOne({ where: { username: req.body.username } });
+  if (!user) return res.status(400).json('Invalid email or password.');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).json('Invalid email or password.');
+
+  const token = jwt.sign({ userId: user.userId, username: user.username, roleId: user.roleId }, 'happy');
+
+  return res.send(token);
+});
+
+function validate(req) {
+  const schema = Joi.object({
+    username: Joi.string().min(5).max(50).required().email(),
+    password: Joi.string().max(255).required(),
+  });
+
+  return schema.validate(req);
+}
--- src/startup/router.js
+++ src/startup/router.js
@@ -2,12 +2,14 @@ const student = require('../routes/student');
 const grading = require('../routes/grading');
 const statistic = require('../routes/statistic');
 const user = require('../routes/user');
+const auth = require('../routes/auth');
 
 module.exports = function (app) {
   app.use('/api/students', student);
   app.use('/api/gradings', grading);
   app.use('/api/statistics', statistic);
   app.use('/api/users', user);
+  app.use('/api/auth', auth);
 
   app.all('*', (req, res, next) => {
     return res.status(404).json({
