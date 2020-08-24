289428987b0c74e5f1a591eee952b202d829af26
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-08-24 14:11:52 +0630

    added bcrypt and hash

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const { User } = require('../database/models');
 const { validate } = require('../database/models/user');
 const catchAsync = require('../utils/catchAsync');
@@ -11,6 +12,8 @@ exports.register = catchAsync(async (req, res) => {
   if (user) return res.status(400).json('User already registered');
 
   user = User.build(_.pick(req.body, ['name', 'username', 'password', 'roleId']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   return res.status(200).json({ status: 'success', data: _.pick(user, ['userId', 'name', 'username']) });
