a875fe007ade3d454e5efcc83fa437487cbb1c16
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-08-24 18:21:30 +0630

    protected routes with auth :tada:

--- src/startup/router.js
+++ src/startup/router.js
@@ -1,3 +1,4 @@
+const authentication = require('../middlewares/auth');
 const student = require('../routes/student');
 const grading = require('../routes/grading');
 const statistic = require('../routes/statistic');
@@ -5,11 +6,19 @@ const user = require('../routes/user');
 const auth = require('../routes/auth');
 
 module.exports = function (app) {
+  /**
+   * * Auth Routes
+   */
+  app.use('/api/users', user);
+  app.use('/api/auth', authentication);
+  // app.use(authentication); // <- enable when all're done.
+
+  /**
+   * * System Related Routes
+   */
   app.use('/api/students', student);
   app.use('/api/gradings', grading);
   app.use('/api/statistics', statistic);
-  app.use('/api/users', user);
-  app.use('/api/auth', auth);
 
   app.all('*', (req, res, next) => {
     return res.status(404).json({
