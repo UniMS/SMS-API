e607bd4cabcb0b93134f6527a7b95a7b911f4540
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2020-12-11 00:22:36 +0630

    enabled cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
